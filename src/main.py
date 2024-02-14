82a2a88c8d71155bbe4909118760458a81edd614
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-02-14 21:51:23 +1100

    Added requirements.txt generation
    Also optimised calls to subprocess.run(), cleaned up progress messages and flipped an if not statement

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import argparse
 import os
-import subprocess
+from subprocess import run
 from jinja2 import Environment, FileSystemLoader
 
 
 # Todo: Split this one big function up into parts
 def create_project(project_name: str, venv_name: str = "venv", packages: list[str] | None = None) -> tuple[bool, str]:
-    print("Creating project folders...", end="", flush=True)
+    print("Creating project folders...", end=" ", flush=True)
     if os.path.exists(project_name):
         return False, "Folder already exists. Try renaming or deleting it."
     os.makedirs(f"{project_name}/src")
-    print(" done.", flush=True)
+    print("done.", flush=True)
 
-    print("Setting up project...", end="", flush=True)
+    print("Setting up project...", end=" ", flush=True)
     jinja = Environment(loader=FileSystemLoader(os.getcwd()))
     template = jinja.get_template("template.jinja2")
     if packages:
@@ -22,23 +22,31 @@ def create_project(project_name: str, venv_name: str = "venv", packages: list[st
 
     with open(f"{project_name}/src/main.py", "w") as f:
         f.write(template)
-    print(" done.", flush=True)
+    print("done.", flush=True)
 
-    print("Setting up virtual environment...", end="", flush=True)
+    print("Setting up virtual environment...", end=" ", flush=True)
     os.chdir(project_name)
-    subprocess.run(["python", "-m", "venv", venv_name], shell=True, check=True)
+    run(["python", "-m", "venv", venv_name], check=True)
     os.chdir(os.path.split(os.getcwd())[0])
-    print(" done.", flush=True)
+    print("done.", flush=True)
 
     if packages:
         print("Installing packages via the virtual environment's pip", flush=True)
         os.chdir(os.path.join(project_name, venv_name, "Scripts"))
         arguments = ["pip.exe", "install"]
         arguments.extend(packages)
-        subprocess.run(arguments, shell=True, check=True)
+        run(arguments, check=True)
+
+        requirements = run(["pip.exe", "freeze"], check=True, capture_output=True, text=True).stdout
 
         for _ in range(2):  # Gets from project\venv\Scripts to project
             os.chdir(os.path.split(os.getcwd())[0])
+        
+        print("Creating requirements file...", end=" ", flush=True)
+        with open("requirements.txt", "w") as f:
+            f.write(requirements)
+        print("done.")
+
         return True, os.getcwd()
 
     return True, os.path.join(os.getcwd(), project_name)
@@ -61,7 +69,7 @@ cli.add_argument(
 if __name__ == "__main__":
     args = cli.parse_args()
     result = create_project(args.project_name, packages=args.dependencies)
-    if not result[0]:
-        raise Exception(result[1])
-    else:
+    if result[0]:
         print(f"Your new project can be found at {result[1]}")
+    else:
+        raise Exception(result[1])
