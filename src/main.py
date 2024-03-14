b7e522305b4cb20a353f533db268e8a455ed303c
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-03-14 21:00:15 +1100

    Refactor to use pathlib
    And FINALLY getting rid of all my temporary debug code

--- src/main.py
+++ src/main.py
@@ -1,32 +1,34 @@
 import argparse
-import os
 from jinja2 import Environment, FileSystemLoader
 from halo import Halo
 from commands import venv, pip_install, pip_freeze
-from sys import exit
+from pathlib import Path
 
 
 def create_project_folders(project_name: str) -> bool:
-    if os.path.exists(project_name):
+    project_path = Path(project_name)
+    if project_path.exists():
         return False
-    os.makedirs(f"{project_name}/src")
+    
+    project_path.mkdir()
+    (project_path / "src").mkdir()
     return True
 
 
 def create_entrypoint(project_name: str, packages: list) -> None:
-    jinja = Environment(loader=FileSystemLoader(os.getcwd()))
+    jinja = Environment(loader=FileSystemLoader(Path.cwd()))
     template = jinja.get_template("template.jinja2")
     if packages:
         template = template.render(dependencies=packages)
     else:
         template = template.render()
 
-    with open(f"{project_name}/src/main.py", "w") as f:
+    with open(Path(project_name) / "src" / "main.py", "w") as f:
         f.write(template)
 
 
 # https://github.com/manrajgrover/halo/issues/5
-def main(project_name: str, venv_name: str, packages: list[str] | None = None) -> tuple[bool, str]:
+def main(project_name: str, venv_name: str, packages: list[str] | None = None) -> tuple[bool, Path]:
     spinner = Halo("Creating project folders...").start()
     success = create_project_folders(project_name)
     if not success:
@@ -40,25 +42,25 @@ def main(project_name: str, venv_name: str, packages: list[str] | None = None) -
 
     if venv_name:
         spinner = Halo("Setting up virtual environment...").start()
-        venv(os.path.join(project_name, venv_name))
+        venv(Path(project_name) / venv_name)
         spinner.succeed(spinner.text + " Done!")
 
     if packages and venv_name:
-        pip_path = os.path.join(project_name, venv_name, "Scripts")
-        pip_executable = os.path.join(pip_path, "pip.exe")
+        pip_path = Path(project_name) / venv_name / "Scripts"
+        pip_executable = Path(pip_path) / "pip.exe"
 
         for package in packages:
             spinner = Halo(f"Installing {package} in {venv_name}...").start()
-            with open(os.path.join(project_name, "pip-log.txt"), "a") as f:
+            with open(Path(project_name) / "pip.exe", "a") as f:
                 pip_install(pip_executable, package, stdout=f, stderr=f, text=True)
             spinner.succeed(spinner.text + " Done!")
 
         spinner = Halo("Creating requirements file...").start()
-        with open(os.path.join(project_name, "requirements.txt"), "w") as f:
+        with open(Path(project_name) / "requirements.txt", "w") as f:
             pip_freeze(pip_executable, stdout=f, stderr=f, text=True)
         spinner.succeed(spinner.text + " Done!")
 
-    return True, os.path.join(os.getcwd(), project_name)
+    return True, Path.cwd() / project_name
 
 
 def setup_cli() -> argparse.ArgumentParser:
