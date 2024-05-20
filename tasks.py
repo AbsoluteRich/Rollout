d07deba15b24d285c726af047df1d3b14943e0a9
Dhu Eid <117572835+AbsoluteRich@users.noreply.github.com>
2024-05-20 19:28:19 +1000

    Version bumper script
    Still won't help me remember to do it though
    ==================================================
    Modified Time     : 20/05/2024 2:52:48 PM

--- tasks.py
+++ tasks.py
@@ -1,4 +1,6 @@
 from invoke import task
+from rollout.__init__ import __version__ as current_version
+from pathlib import Path
 
 heavy_commands = [
     "pipenv update",
@@ -13,10 +15,40 @@ commands = [
 
 
 @task
-def pc(c, lite=False):
+def pc(c, lite: bool = False):
     if not lite:
         for command in heavy_commands:
             c.run(command)
 
     for command in commands:
         c.run(command)
+
+
+@task
+def bump(_, label: str = "minor"):
+    version_file = Path.cwd() / "rollout" / "__init__.py"
+    new_version = current_version.split(".")
+
+    match label:
+        case "major":
+            location = 0
+        case "minor":
+            location = 1
+        case "patch":
+            location = 2
+        case _:
+            print("Invalid semver label!")
+            return
+
+    new_version[location] = str(int(new_version[location]) + 1)
+    new_version = ".".join(new_version)
+
+    print(f"Current version: {current_version}")
+    print(f"Bumped version: {new_version}")
+
+    with open(version_file, "r") as f:
+        content = f.read()
+
+    with open(version_file, "w") as f:
+        content = content.replace(current_version, new_version)
+        f.write(content)
